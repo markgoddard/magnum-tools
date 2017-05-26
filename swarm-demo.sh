5f8b8aa8d85eaa6e443fcb963695368580da588e
Mark Goddard <mark@stackhpc.com>
2017-05-26 11:59:29 +0100

    Add support for swarm mode to swarm demo script

--- swarm-demo.sh
+++ swarm-demo.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -e
 
+# Script demoing the use of a docker swarm cluster which has been deployed
+# via OpenStack magnum.
+
+# These IPs should be set to the floating IP addresses of the Swarm nodes.
+FIPS="${FIPS:-10.60.253.17 10.60.253.35 10.60.253.29}"
+OPENRC_FILE="/ilab-home/hpcgodd1/mark-openrc.sh"
+VENV="/ilab-home/hpcgodd1/os-venv"
+STACK="mark-swarm-fedora-25"
+
 function announce {
     >&2 echo -e "\e[33m$*\e[39m"
 }
@@ -14,8 +23,6 @@ function pause {
     read
 }
 
-FIPS="10.60.253.20 10.60.253.25 10.60.253.29"
-
 announce "Demo: Docker swarm on OpenStack magnum!"
 read
 
@@ -29,23 +36,32 @@ export PATH=${PATH}:$(pwd)/docker
 pause
 
 announce "Getting cluster configuration from magnum API"
-source ~/os-venv/bin/activate
-source ~/mark-openrc.sh
-run magnum cluster-config mark-swarm-fedora-25 > swarm-env
+source "${VENV}/bin/activate"
+source "${OPENRC_FILE}"
+run magnum cluster-config ${STACK} > swarm-env
 deactivate
 ls -l
 pause
 
 announce "Display dowloaded cluster configuration environment"
 run cat swarm-env
+source swarm-env
 pause
+
+if docker info | grep 'Swarm: active' >/dev/null; then
+    SWARM_MODE=1
+fi
 
 announce "Creating a docker overlay network"
 run docker network create overlay-net --driver overlay --subnet 10.0.1.0/24
 pause
 
 announce "Creating 3 nginx containers"
-for i in $(seq 0 2) ; do run docker run -d -p 8080:80 --name nginx-$i --net overlay-net nginx; done
+if [[ $SWARM_MODE -eq 1 ]]; then
+    run docker service create -p 8080:80 --name nginx --network overlay-net --replicas 3 nginx
+else
+    for i in $(seq 0 2) ; do run docker run -d -p 8080:80 --name nginx-$i --net overlay-net nginx; done
+fi
 run docker ps
 pause
 
