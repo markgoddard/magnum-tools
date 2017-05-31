2abe974b7dd3e27c746c8a70ac439f8a28ee8d88
Mark Goddard <mark@stackhpc.com>
2017-05-31 12:15:25 +0100

    Rename build-image.sh to build-swarm-image.sh, update for docker CE

--- build-swarm-image.sh
+++ build-swarm-image.sh
@@ -3,9 +3,6 @@
 # Build and/or register an image for use with OpenStack magnum using docker
 # swarm on fedora 25 on baremetal.
 
-# Currently the magnum kubernetes baremetal DIB element is being used as
-# there is no DIB element for swarm on fedora on baremetal.
-
 # Partition (not whole disk) images are recommended as they allow for the use
 # of an ephemeral partition on the instance which can be used by magnum for
 # docker storage.
@@ -26,62 +23,72 @@ else
     REGISTER=1
 fi
 
+if [[ ! -d stackhpc-image-elements ]]; then
+    git clone https://github.com/stackhpc/stackhpc-image-elements
+fi
+
 virtualenv dib-venv
 source dib-venv/bin/activate
 pip install -U pip
-pip install git+https://github.com/stackhpc/diskimage-builder@stackhpc-2.3.3
+# Master is required for dracut-regenerate
+pip install git+https://github.com/openstack/diskimage-builder@master
 pip install dib-utils python-glanceclient
 
-if [[ ! -d magnum ]]; then
-    git clone https://git.openstack.org/openstack/magnum
-fi
-export ELEMENTS_PATH=magnum/magnum/drivers/k8s_fedora_ironic_v1/image
+export ELEMENTS_PATH=$(pwd)/stackhpc-image-elements/elements
 export DIB_RELEASE=25
+# Enable the [cutting] edge repository.
+export DIB_DOCKER_ENABLED_REPOS=docker-ce-edge
 
 # The following is required for --visibility and --os-distro arguments.
 export OS_IMAGE_API_VERSION=2
 
-NAME=${NAME:-swarm-fedora-${DIB_RELEASE}}
+NAME=${NAME:-swarm-ce-fedora-${DIB_RELEASE}}
+FILENAME=${FILENAME:-$NAME}
 USE_WHOLE_DISK_IMAGE=${USE_WHOLE_DISK_IMAGE:-0}
 
 if [[ $BUILD -eq 1 ]]; then
+    echo "Building image"
     if [[ $USE_WHOLE_DISK_IMAGE -eq 0 ]]; then
         disk-image-create \
           baremetal \
           dhcp-all-interfaces \
-          dracut-network \
+          dracut-regenerate \
           enable-serial-console \
           fedora \
           grub2 \
           selinux-permissive \
-          kubernetes \
-          -o fedora-${DIB_RELEASE}.qcow2
+          docker-ce \
+          magnum-swarm \
+          -o ${FILENAME}.qcow2
     else
         disk-image-create \
           dhcp-all-interfaces \
-          dracut-network \
+          dracut-regenerate \
           enable-serial-console \
           fedora \
           selinux-permissive \
           vm \
-          kubernetes \
-          -o fedora-${DIB_RELEASE}.qcow2
+          docker-ce \
+          magnum-swarm \
+          -o ${FILENAME}.qcow2
     fi
+    echo "Built image"
 fi
 
 if [[ $REGISTER -eq 1 ]]; then
+    echo "Registering images"
     if [[ $USE_WHOLE_DISK_IMAGE -eq 0 ]]; then
         KERNEL_ID=`glance image-create --name ${NAME}-kernel \
                                        --visibility public \
                                        --disk-format=aki \
                                        --container-format=aki \
-                                       --file=fedora-${DIB_RELEASE}.vmlinuz \
+                                       --file=${FILENAME}.vmlinuz \
                                        | grep id | tr -d '| ' | cut --bytes=3-57`
         RAMDISK_ID=`glance image-create --name ${NAME}-ramdisk \
                                         --visibility public \
                                         --disk-format=ari \
                                         --container-format=ari \
-                                        --file=fedora-${DIB_RELEASE}.initrd \
+                                        --file=${FILENAME}.initrd \
                                         | grep id |  tr -d '| ' | cut --bytes=3-57`
         BASE_ID=`glance image-create --name ${NAME} \
                                         --os-distro fedora \
@@ -90,7 +97,7 @@ if [[ $REGISTER -eq 1 ]]; then
                                         --container-format=bare \
                                         --property kernel_id=$KERNEL_ID \
                                         --property ramdisk_id=$RAMDISK_ID \
-                                        --file=fedora-${DIB_RELEASE}.qcow2 \
+                                        --file=${FILENAME}.qcow2 \
                                         | grep -v kernel | grep -v ramdisk \
                                         | grep id | tr -d '| ' | cut --bytes=3-57`
     else
@@ -99,8 +106,9 @@ if [[ $REGISTER -eq 1 ]]; then
                                         --visibility public \
                                         --disk-format=qcow2 \
                                         --container-format=bare \
-                                        --file=fedora-${DIB_RELEASE}.qcow2 \
+                                        --file=${FILENAME}.qcow2 \
                                         | grep -v kernel | grep -v ramdisk \
                                         | grep id | tr -d '| ' | cut --bytes=3-57`
     fi
+    echo "Registered images"
 fi
