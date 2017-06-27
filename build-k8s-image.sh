d02114a4bccb23158ec3eb780878b4f2fc7447b2
Mark Goddard <mark@stackhpc.com>
2017-06-27 11:19:22 +0100

    Support fedora atomic images in build-k8s-image.sh

--- build-k8s-image.sh
+++ build-k8s-image.sh
@@ -26,46 +26,67 @@ fi
 virtualenv dib-venv
 source dib-venv/bin/activate
 pip install -U pip
-pip install git+https://github.com/openstack/diskimage-builder@master
+pip install git+https://github.com/stackhpc/diskimage-builder@stackhpc-2.3.3
 pip install dib-utils python-glanceclient
 
 if [[ ! -d magnum ]]; then
     git clone https://git.openstack.org/openstack/magnum
 fi
-export ELEMENTS_PATH=magnum/magnum/drivers/k8s_fedora_ironic_v1/image
-export DIB_RELEASE=25
+export ELEMENTS_PATH=magnum/magnum/drivers/k8s_fedora_ironic_v1/image:magnum/magnum/drivers/common/image
 
 # The following is required for --visibility and --os-distro arguments.
 export OS_IMAGE_API_VERSION=2
 
-NAME=${NAME:-k8s-fedora-${DIB_RELEASE}}
+# Supports 'fedora' and 'fedora-atomic'.
+OS_DISTRO=${OS_DISTRO:-fedora}
+case $OS_DISTRO in
+    fedora | fedora-atomic)
+        DIB_RELEASE=${DIB_RELEASE:-25}
+        ;;
+    *)
+        echo "Unsupported OS_DISTRO: $OS_DISTRO"
+        ;;
+esac
+export DIB_RELEASE
+NAME=${NAME:-k8s-${OS_DISTRO}-${DIB_RELEASE}}
 FILENAME=${FILENAME:-$NAME}
 USE_WHOLE_DISK_IMAGE=${USE_WHOLE_DISK_IMAGE:-0}
 
 if [[ $BUILD -eq 1 ]]; then
     echo "Building image"
+    ELEMENTS="dhcp-all-interfaces \
+enable-serial-console \
+selinux-permissive"
+
     if [[ $USE_WHOLE_DISK_IMAGE -eq 0 ]]; then
-        disk-image-create \
-          baremetal \
-          dhcp-all-interfaces \
-          dracut-regenerate \
-          enable-serial-console \
-          fedora \
-          grub2 \
-          selinux-permissive \
-          kubernetes \
-          -o ${FILENAME}.qcow2
+        ELEMENTS="$ELEMENTS baremetal grub2"
     else
-        disk-image-create \
-          dhcp-all-interfaces \
-          dracut-regenerate \
-          enable-serial-console \
-          fedora \
-          selinux-permissive \
-          vm \
-          kubernetes \
-          -o ${FILENAME}.qcow2
+        ELEMENTS="$ELEMENTS vm"
     fi
+
+    # Use dracut-regenerate if possible, but it was only added recently.
+    if [[ -f dib-venv/lib/python2.7/site-packages/diskimage_builder/elements/dracut-regenerate ]]; then
+        ELEMENTS="$ELEMENTS dracut-regenerate"
+    else
+        ELEMENTS="$ELEMENTS dracut-network"
+    fi
+
+    case $OS_DISTRO in
+        fedora)
+            ELEMENTS="$ELEMENTS fedora kubernetes"
+            ;;
+        fedora-atomic)
+            ELEMENTS="$ELEMENTS fedora-atomic"
+            export DIB_IMAGE_SIZE=2.5
+            # Without these the select-boot-kernel-initrd element fails as the
+            # ramdisk and kernel are not in /boot. This element is a dependency
+            # of dracut-network/dracut-regenerate.
+            export DIB_BAREMETAL_KERNEL_PATTERN='ostree/fedora-atomic-*/vmlinuz*'
+            export DIB_BAREMETAL_INITRD_PATTERN='ostree/fedora-atomic-*/initramfs-*'
+            ;;
+    esac
+
+    disk-image-create $ELEMENTS -o ${FILENAME}.qcow2 -x
     echo "Built image"
 fi
 
@@ -96,7 +117,7 @@ if [[ $REGISTER -eq 1 ]]; then
                                         | grep id | tr -d '| ' | cut --bytes=3-57`
     else
         BASE_ID=`glance image-create --name ${NAME} \
-                                        --os-distro fedora \
+                                        --os-distro ${OS_DISTRO} \
                                         --visibility public \
                                         --disk-format=qcow2 \
                                         --container-format=bare \
@@ -105,4 +126,7 @@ if [[ $REGISTER -eq 1 ]]; then
                                         | grep id | tr -d '| ' | cut --bytes=3-57`
     fi
     echo "Registered images"
+    echo "Kernel: $KERNEL_ID"
+    echo "Ramdisk: $RAMDISK_ID"
+    echo "Image: $BASE_ID"
 fi
